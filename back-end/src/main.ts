ed0b9d125ededdfd6ea396006e6aa23a49ddadce
nafef60552 <nafef60552@qianhost.com>
2023-11-16 21:40:18 +0200

    minor changes

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.useLogger(app.get(Logger));
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
