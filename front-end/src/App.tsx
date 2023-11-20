b3f581a951ebc09b51b9ebece1b842b1953cc12a
nafef60552 <nafef60552@qianhost.com>
2023-11-20 18:12:14 +0200

    tmp changes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,57 @@
-import { Login } from "./components/Login/Login";
+import React, { FC, useContext, useEffect, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { Context } from "./main";
+import Cookies from "js-cookie";
+import { GiftCertificate } from "./types/gift-certeficates/GiftCertificates";
+import GiftCertificateService from "./http/service/GiftCertificatesService";
 
-function App() {
+const App: FC = () => {
+  const { store } = useContext(Context);
+  const [certeficates, setCerteficates] = useState<GiftCertificate[]>([]);
+
+  useEffect(() => {
+    if (Cookies.get("refreshToken")) {
+      store.checkAuth();
+    }
+  }, []);
+
+  async function getGiftCertificates() {
+    try {
+      const response = await GiftCertificateService.getAll();
+      console.log(response);
+      setCerteficates(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <Login isLogin={false}/>
+    <div>
+      {/* <h1>{store.isAuth ? `Пользователь авторизован ${store.user.email}` : 'АВТОРИЗУЙТЕСЬ'}</h1> */}
+      <button onClick={() => store.logout()}>Logout</button>
+      <div>
+        <button onClick={getGiftCertificates}>Get certeficates</button>
+      </div>
+      {certeficates.map((certificate) => (
+        <div key={certificate.code}>
+          <div>{certificate.maximumUsage}</div>
+          <div>{certificate.code}</div>
+          {certificate.tags.map((certificate) => (
+            <div key={certificate.id}>
+              <div>{certificate.id}</div>
+              <div>{certificate.name}</div>
+            </div>
+          ))}
+          <br />
+          <br />
+        </div>
+      ))}
+    </div>
   );
-}
+};
 
-export default App;
+export default observer(App);
