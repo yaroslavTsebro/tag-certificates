0e157f46bb92df9d10c7e2ee0cf06caf46234332
nafef60552 <nafef60552@qianhost.com>
2023-11-20 18:06:44 +0200

    added transformation for fields with underscores

--- back-end/libs/common/src/database/base.entity.ts
+++ back-end/libs/common/src/database/base.entity.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from 'class-transformer';
 import {
   CreateDateColumn,
   PrimaryGeneratedColumn,
@@ -16,5 +17,31 @@ export abstract class BaseEntity<T> {
 
   constructor(entity: Partial<T>) {
     Object.assign(this, entity);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  toJSON(): {} {
+    const record = instanceToPlain(this);
+
+    // Remove all double underscores `__` from all properties when serializing object
+    const cleanUnderscoresProperties = (obj: object): void => {
+      for (const [key, val] of Object.entries(obj)) {
+        if (key.startsWith('__') && key.endsWith('__')) {
+          const newKey = key.substring(2, key.length - 2);
+          obj[newKey] = obj[key];
+          delete obj[key];
+        } else if (typeof val === 'object') {
+          if (val != null) {
+            cleanUnderscoresProperties(val);
+          }
+        }
+      }
+    };
+
+    if (record != null) {
+      cleanUnderscoresProperties(record);
+    }
+
+    return record;
   }
 }
