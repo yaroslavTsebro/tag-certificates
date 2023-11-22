c1c838e19e36d6536822830fcedcec7de8e5a23f
nafef60552 <nafef60552@qianhost.com>
2023-11-22 19:05:32 +0200

    added header

---
+++ front-end/src/components/Header/Header.tsx
@@ -0,0 +1,58 @@
+import { Form, NavLink, useRouteLoaderData } from "react-router-dom";
+import classes from './MainNavigation.module.scss';
+
+
+function MainNavigation() {
+  const token = useRouteLoaderData('root');
+
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/certificates"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Certificates
+            </NavLink>
+          </li>
+          {!token && (
+            <li>
+              <NavLink
+                to="/auth?mode=login"
+                className={({ isActive }) =>
+                  isActive ? classes.active : undefined
+                }
+              >
+                Authentication
+              </NavLink>
+            </li>
+          )}
+          {token && (
+            <li>
+              <Form action="/logout" method="post">
+                <button>Logout</button>
+              </Form>
+            </li>
+          )}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default MainNavigation;
