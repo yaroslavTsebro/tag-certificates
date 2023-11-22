2ee28f01bc81ee3add863444ec49def0b085159f
nafef60552 <nafef60552@qianhost.com>
2023-11-22 19:06:34 +0200

    added home page

---
+++ front-end/src/components/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import PageContent from "../../common/PageContent/PageContent";
+
+function HomePage() {
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing events!</p>
+    </PageContent>
+  );
+}
+
+export default HomePage;
