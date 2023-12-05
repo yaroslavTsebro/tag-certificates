abf396cffad2d61ce01af87ce898c74090ae4436
yaroslavTsebro <yaroslav.cebro@gmail.com>
2023-12-05 19:22:01 +0200

    added methods

--- back-end/src/auth/user/service/users-service.interface.ts
+++ back-end/src/auth/user/service/users-service.interface.ts
@@ -1,12 +1,18 @@
 import { CreateUserDto } from '../entity/dto/create-user.dto';
-import User from '../entity/user.entity';
+import { GoogleProfileDto } from '../entity/dto/oauth/google-profile.dto';
+import User, { AuthType } from '../entity/user.entity';
 
 export const USER_SERVICE = Symbol('IUserService');
 
 export interface IUsersService {
   create(dto: CreateUserDto): Promise<User>;
+  createFromOauth(authType: AuthType, profile: GoogleProfileDto): Promise<User>;
   verifyUser(email: string, password: string): Promise<User>;
   getById(id: number): Promise<User>;
   changeUsername(id: number, username: string): Promise<User>;
   delete(id: number): Promise<void>;
+  getUserByAuthTypeAndClientId(
+    authType: AuthType,
+    clientId: string,
+  ): Promise<User | null>;
 }
--- back-end/src/auth/user/service/users.service.ts
+++ back-end/src/auth/user/service/users.service.ts
@@ -6,15 +6,25 @@ import {
 } from '@nestjs/common';
 import { IUsersService } from './users-service.interface';
 import { CreateUserDto } from '../entity/dto/create-user.dto';
-import User, { UserRole } from '../entity/user.entity';
+import User, { AuthType, UserRole } from '../entity/user.entity';
 import * as bcrypt from 'bcryptjs';
 import { UsersRepository } from '../repository/users.repository';
+import { GoogleProfileDto } from '../entity/dto/oauth/google-profile.dto';
 
 @Injectable()
 export class UsersService implements IUsersService {
   constructor(
     @Inject(UsersRepository) private readonly usersRepository: UsersRepository,
   ) {}
+  async getUserByAuthTypeAndClientId(
+    authType: AuthType,
+    clientId: string,
+  ): Promise<User | null> {
+    return await this.usersRepository.findOne({
+      authType: authType,
+      oauthId: clientId,
+    });
+  }
 
   async delete(id: number): Promise<void> {
     return await this.usersRepository.findOneAndDelete({ id });
@@ -43,6 +53,20 @@ export class UsersService implements IUsersService {
       ...createUserDto,
       role: UserRole.USER,
       password: await bcrypt.hash(createUserDto.password, 10),
+    });
+    return this.usersRepository.create(user);
+  }
+
+  async createFromOauth(
+    authType: AuthType,
+    profile: GoogleProfileDto,
+  ): Promise<User> {
+    const user = new User({
+      username: profile.username,
+      authType: authType,
+      email: profile.email,
+      role: UserRole.USER,
+      oauthId: profile.clientId,
     });
     return this.usersRepository.create(user);
   }
